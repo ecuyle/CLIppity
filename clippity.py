6760503e8d006163e2aa43ccc46339d276eec4e1
Eric Cuyle <ecuyle@gmail.com>
2023-08-19 20:37:11 +0200

    Cleans up conversation summary print statements

--- clippity.py
+++ clippity.py
@@ -12,7 +12,7 @@ token_usage = {"prompt_tokens": 0, "completion_tokens": 0, "total_tokens": 0}
 
 def calculate_token_cost(tokens):
     dollarPerToken = 0.000002
-    return '${:,.7f}'.format(tokens * dollarPerToken)
+    return "${:,.7f}".format(tokens * dollarPerToken)
 
 
 def get_response(prompt_text):
@@ -39,17 +39,19 @@ def main():
     while True:
         prompt = input("> ")
         if prompt.lower() in ["exit", "quit"]:
-            print("--------------------------------------------------")
-            print("Conversation Summary:")
-            print("--------------------------------------------------")
-            print("Token Usage")
-            print(f"  Prompt Tokens:     {token_usage['prompt_tokens']}")
-            print(f"  Completion Tokens: {token_usage['completion_tokens']}")
-            print(f"  Total Tokens:      {token_usage['total_tokens']}")
-            print(
-                f"Total Cost: {calculate_token_cost(token_usage['total_tokens'])}")
-            print("\n")
-            print("Goodbye!")
+            print(f"""
+--------------------------------------------------
+Conversation Summary:
+--------------------------------------------------
+Token Usage
+  Prompt Tokens:     {token_usage['prompt_tokens']}
+  Completion Tokens: {token_usage['completion_tokens']}
+  Total Tokens:      {token_usage['total_tokens']}
+Total Cost: {calculate_token_cost(token_usage['total_tokens'])}
+
+Goodbye!
+                  """)
+
             sys.exit()
 
         response = get_response(prompt)
